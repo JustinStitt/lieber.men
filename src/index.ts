588ad194cf2c716450e65d2a2b2c50af44715748
Justin Stitt <jstitt007@gmail.com>
2023-02-12 23:17:31 -0800

    remove db

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import * as dotenv from 'dotenv';
-import { query } from './db';
 dotenv.config()
 
 const app = express();
@@ -21,19 +20,12 @@ app.get('/leaderboard', (req, res) => {
 });
 
 app.get('/api/leaderboard', async (req, res) => {
-  const data = await query(
-    "SELECT Links.createdBy as user, count(Clicks.userName) numClicks FROM Clicks INNER JOIN Links ON Links.name = Clicks.linkName GROUP BY Links.createdBy ORDER BY count(Clicks.userName) DESC;"
-    , []).catch((e) => {})
-  res.json(data);
+  return {'hello': 'world'};
 })
 
 app.post('/generate_url', async (req, res) => {
   const body = req.body;
   body.url = encodeURIComponent(body.url)
-  await query("INSERT INTO Users (name) VALUES (?);", [body.name]).catch((e) => {})
-  await query("INSERT INTO Links (name, createdBy) VALUES (?, ?);", [body.url, body.name]).catch((e) => {
-    res.status(400).send("Link already exists")
-  })
   if (res.headersSent) return;
   res.json({
     url: `aaron.lieber.men/sus/${body.url}`
@@ -41,9 +33,7 @@ app.post('/generate_url', async (req, res) => {
 });
 
 app.get('/sus/:id', async (req, res) => {
-  if (req.cookies.pass && req.cookies.pass === process.env.PHISHING_PASSWORD) {
-    await query("INSERT INTO Clicks (userName, linkName) VALUES ('Aaron', ?);", [req.params.id])
-  }
+  console.log('password entered: ', req.cookies.pass)
   res.sendFile(path.join(__dirname + '/../public/youJustGotPhished.html'));
 });
 
